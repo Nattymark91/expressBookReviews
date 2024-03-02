cd034bce86fbd50747a8a8b5e98ae15f7e8d0b7f
Nattymark91 <157714294+Nattymark91@users.noreply.github.com>
2024-03-02 19:45:12 +0200

    promise general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -20,57 +20,113 @@ public_users.post("/register", (req,res) => {
         {
              return res.status(403).json({message: "Username already exists"});
         }
-    }
-    else {
+    } else {
       return res.status(400).json({message: "All fields are required to be filled in"});
     }
 });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-    return res.status(200).send(JSON.stringify(books));
+
+    const promise = new Promise((resolve,reject)=>{
+
+        if(books){
+          resolve(books)
+        }
+        else
+        {
+          reject("Books not found")
+        }
+      })
+    
+      promise
+       .then( response =>{ return res.status(200).send(JSON.stringify(response));})
+       .catch( err =>res.status(400).json(err))
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-    const isbn = req.params.isbn;
-    return res.status(200).send(JSON.stringify(books[isbn]));
+
+    const promise = new Promise((resolve,reject)=>{
+        const isbn = req.params.isbn;
+        if(books[isbn]){
+          resolve(books[isbn])
+        }
+        else
+        {
+          reject("Book not found")
+        }
+      })
+    
+      promise
+       .then( response =>{ return res.status(200).send(JSON.stringify(response));})
+       .catch( err =>res.status(400).json(err))
+
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
+
     const author = req.params.author;
-  let booksbyauthor = new Array();
-  for (let key in books) {
-    const el = books[key];
-    if (el.author == author) {
-        const item = {
-            "isbn" : key,
-            "title" : el.title,
-            "reviews": el. reviews
+    let booksbyauthor = new Array();
+
+    const promise = new Promise((resolve,reject)=>{
+        for (let key in books) {
+        const el = books[key];
+            if (el.author == author) {
+                const item = {
+                    "isbn" : key,
+                    "title" : el.title,
+                    "reviews": el. reviews
+                }
+        booksbyauthor.push(item);
+            }
         }
-      booksbyauthor.push(item);
-    }
-  }
-  return res.status(200).send(JSON.stringify({ booksbyauthor: booksbyauthor }));
+        if(booksbyauthor.length > 0){
+            resolve(booksbyauthor)
+        }
+        else
+        {
+          reject("Book not found")
+        }
+      })
+    
+      promise
+       .then( response =>{ return res.status(200).send(JSON.stringify({ booksbyauthor: response }));})
+       .catch( err =>res.status(400).json(err))
+
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
+
     const title = req.params.title;
     let booksbytitle = new Array();
-    for (let key in books) {
-      const el = books[key];
-      if (el.title == title) {
-          const item = {
-              "isbn" : key,
-              "author" : el.author,
-              "reviews": el. reviews
-          }
-          booksbytitle.push(item);
-      }
-    }
-    return res.status(200).send(JSON.stringify({ booksbytitle: booksbytitle }));
+
+    const promise = new Promise((resolve,reject)=>{
+        for (let key in books) {
+        const el = books[key];
+            if (el.title == title) {
+                      const item = {
+                          "isbn" : key,
+                          "author" : el.author,
+                          "reviews": el. reviews
+                      }
+                      booksbytitle.push(item);
+            }
+        }
+        if(booksbytitle.length > 0){
+            resolve(booksbytitle)
+        }
+        else
+        {
+          reject("Book not found")
+        }
+      })
+    
+      promise
+       .then( response =>{ return res.status(200).send(JSON.stringify({ booksbytitle: response }));})
+       .catch( err =>res.status(400).json(err))
 });
 
 //  Get book review
